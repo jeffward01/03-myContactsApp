9cc1c9e6adad1bcb5bf9420d3e315b8a0d52fd44
Jeff Ward <jeffwardcoding@gmail.com>
2016-01-03 22:44:10 -0800

    wiring up angular

---
+++ app/contacts/contacts.js
@@ -0,0 +1,132 @@
+'use strict';
+
+angular.module('myContacts.contacts', ['ngRoute', 'firebase'])
+
+.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/contacts', {
+        templateUrl: 'contacts/contacts.html',
+        controller: 'ContactsController'
+    });
+}])
+
+
+//Contacts Controller
+.controller('ContactsController', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+
+    //Init Firbase
+    var ref = new Firebase('https://mycontactportal.firebaseio.com/contacts');
+
+    //Get Cintacts
+    $scope.contacts = $firebaseArray(ref);
+
+    //Show Add form
+    $scope.showAddForm = function () {
+        $scope.addFormShow = true;
+    }
+
+    //Hide Add form
+    $scope.hide = function () {
+        $scope.addFormShow = false;
+    }
+
+
+    $scope.addFormSubmit = function () {
+        console.log('Adding Contact....');
+
+
+        //Assign Values
+        if ($scope.name) {
+            var name = $scope.name;
+        } else {
+            var name = null;
+        }
+        if ($scope.email) {
+            var email = $scope.email;
+        } else {
+            var email = null;
+        }
+        if ($scope.company) {
+            var company = $scope.company;
+        } else {
+            var company = null;
+        }
+        if ($scope.mobile_phone) {
+            var mobile_phone = $scope.mobile_phone;
+        } else {
+            var mobile_phone = null;
+        }
+        if ($scope.home_phone) {
+            var home_phone = $scope.home_phone
+        } else {
+            var home_phone = null
+        }
+        if ($scope.work_phone) {
+            var work_phone = $scope.work_phone;
+        } else {
+            var work_phone = null;
+        }
+        if ($scope.street_address) {
+            var street_address = $scope.street_address;
+        } else {
+            var street_address = null;
+        }
+        if ($scope.city) {
+            var city = $scope.city;
+        } else {
+            var city = null;
+        }
+        if ($scope.state) {
+            var state = $scope.state;
+        } else {
+            var state = null;
+        }
+        if ($scope.zipcode) {
+            var zipcode = $scope.zipcode;
+        } else {
+            var zipcode = null;
+        }
+
+        
+        //Build Object
+        $scope.contacts.$add({
+            name: name,
+            email: email,
+            company: company,
+            phones:[
+                {
+                    mobile: mobile_phone,
+                    home: home_phone,
+                    work: work_phone
+                }
+            ],
+            address: [
+                {
+                    street_address: street_address,
+                    city: city,
+                    state: state,
+                    zipcode: zipcode
+                }
+            ]
+        }).then(function(ref){
+            //assign a key | add contact with ID
+            var id = ref.key();
+            console.log("Added contact with ID " + id);
+            
+            //Clear Form
+            clearFields();
+            
+            //Hide form
+            $scope.addFormShow = false;
+            
+            //Send Message
+            $scope.msg = "Contact Added!"
+            
+            
+            
+            
+        })
+
+
+    }
+
+}]);
